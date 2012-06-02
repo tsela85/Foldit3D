843bd5146c94477f85576123d1295973e010edb5
tzipi alashvili <tzipiala@gmail.com>
2012-06-02 19:26:30 +0300

    texturos new

--- WindowsGame3/WindowsGame3/Game1.cs
+++ WindowsGame3/WindowsGame3/Game1.cs
@@ -66,7 +66,7 @@ namespace Foldit3D
             spriteBatch = new SpriteBatch(GraphicsDevice);
             SpriteFont font = Content.Load<SpriteFont>("font");
             SpriteFont scoreFont = Content.Load<SpriteFont>("scoreFont");
-            HoleManager holeManager = new HoleManager(Content.Load<Texture2D>("hole1"), Content.Load<Effect>("effects"));
+            HoleManager holeManager = new HoleManager(Content.Load<Texture2D>("hole2"), Content.Load<Effect>("effects"));
             PlayerManager playerManager = new PlayerManager(Content.Load<Texture2D>("gummy2"), Content.Load<Effect>("effects"));
             PowerUpManager powerupManager = new PowerUpManager(Content.Load<Texture2D>("inkspot"), Content.Load<Effect>("effects"));
             Board board = new Board(Content.Load<Texture2D>("paper"), Content.Load<Effect>("effects"));
